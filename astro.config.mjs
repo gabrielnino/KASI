cc4fa0f711bf05ab7a549037ffb02b12bed76729
luisg <gabrielnino@gmail.com>
2026-06-30 13:30:56 -0700

    feat: install and configure @astrojs/sitemap and add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kasi.systems',
+  integrations: [sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'zh', 'pa'],
